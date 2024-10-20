2af90d01a62418103221abb05216703e62f3a7e0
CarlosHenrique1996 <carlossouzarodrigues@live.com>
2024-10-20 16:26:55 -0300

    new scenarios

---
+++ teste de automacao/cypress/e2e/step_definitions/product_API/product_API.js
@@ -0,0 +1,30 @@
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+
+
+// Procure por um produto
+Given('o usuario deseja buscar um produto especifico', () => {
+    
+})
+
+When('o usuário digitar o nome do produto na barra de busca', () => {
+
+})
+
+Then('o sistema deve apresentar os resultados de acordo com o que foi informado na busca', () => {
+
+})
+
+
+
+// Atualize a imagem de um produto
+Given('o usuario deseja alterar a imagem do produto', () => {
+
+})
+
+When('o usuario fizer upload da imagem desejada', () => {
+
+})
+
+Then('o sistema deve confirmar se a alteração foi efetuada com sucesso', () => {
+
+})
